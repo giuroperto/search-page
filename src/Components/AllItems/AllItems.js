b399e6157d84b4a69f2e4cb8c24239b6195a18db
Giu Roperto <giu.roperto@gmail.com>
2020-10-01 03:39:05 -0300

    created delivery menu buttons and conditional rendering - still need to format checked btn

--- src/Components/AllItems/AllItems.js
+++ src/Components/AllItems/AllItems.js
@@ -11,6 +11,7 @@ const ListaDeliveryItems = () => {
   const [ searchWord, setSearchWord ] = useState("");
   const [ search, setSearch ] = useState(false);
   const [ pageHeight, setPageHeight ] = useState(0);
+  const [ type, setType ] = useState("DELIVERY");
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -39,14 +40,26 @@ const ListaDeliveryItems = () => {
     setPageHeight(document.documentElement.scrollTop);
   };
 
+  const changeType = (e) => {
+    e.preventDefault();
+
+    const { value } = e.target;
+
+    setType(value);
+  };
+
   return(
     <div className="lista-items-delivery-container">
       <div className="header-lista-items-delivery">
         <Link to="/"><img className="logo" src="https://res.cloudinary.com/dxatyucj2/image/upload/v1597523783/Cantina/removebg-logo_yvoogc.png" alt="Logo Empório Roperto"/></Link>
         <div className="title">
-          <h2>CONSULTAR ITEM (DELIVERY)</h2>
-          <FaMotorcycle className="icon"/>
+          <h2>CONSULTAR ITEM ( {type} )</h2>
+          { type === 'DELIVERY' && <FaMotorcycle className="icon"/> }
         </div>
+      </div>
+      <div className="toggle-btn">
+        <input type="radio" name="type" value="DELIVERY" onClick={changeType} />
+        <input type="radio" name="type" value="SALÃO" onClick={changeType} />
       </div>
       <div className="search-bar">
         <input type="text" value={searchWord} placeholder="Digite o nome do item" onChange={handleChange} />
@@ -55,21 +68,42 @@ const ListaDeliveryItems = () => {
       <div className="page-style">
       {
         search ? (
-          DadosDeliveryItems.filter((item) => (item.ACTIVE === "TRUE" || item.ACTIVE === "VERDADEIRO") && (item.PRICE_DELIVERY !== 0) && ((item.ITEM.toLowerCase().includes(searchWord.toLowerCase())) || (item.GROUP.toLowerCase().includes(searchWord.toLowerCase())))).map((item, idx) => (
-            <div className="item" key={idx}>
-              <h4>{item.ITEM}</h4>
-              <p>{item.GROUP}</p>
-              <p className="price">R$ {item.PRICE_DELIVERY}</p>
-            </div>
-          ))
+          type === 'DELIVERY' ? (
+            DadosDeliveryItems.filter((item) => (item.ACTIVE === "TRUE" || item.ACTIVE === "VERDADEIRO") && (item.PRICE_DELIVERY !== 0) && ((item.ITEM.toLowerCase().includes(searchWord.toLowerCase())) || (item.GROUP.toLowerCase().includes(searchWord.toLowerCase())))).map((item, idx) => (
+              <div className="item" key={idx}>
+                <h4>{item.ITEM}</h4>
+                <p>{item.GROUP}</p>
+                <p className="price">R$ {item.PRICE_DELIVERY}</p>
+              </div>
+            ))
+          ) : (
+            DadosDeliveryItems.filter((item) => (item.ACTIVE === "TRUE" || item.ACTIVE === "VERDADEIRO") && (item.PRICE_MENU !== 0) && ((item.ITEM.toLowerCase().includes(searchWord.toLowerCase())) || (item.GROUP.toLowerCase().includes(searchWord.toLowerCase())))).map((item, idx) => (
+              <div className="item" key={idx}>
+                <h4>{item.ITEM}</h4>
+                <p>{item.GROUP}</p>
+                <p className="price">R$ {item.PRICE_MENU}</p>
+              </div>
+            ))
+          )
         ) : (
-          DadosDeliveryItems.filter((item) => (item.ACTIVE === "TRUE" || item.ACTIVE === "VERDADEIRO") && (item.PRICE_DELIVERY !== 0)).map((item, idx) => (
-            <div className="item" key={idx}>
-              <h4>{item.ITEM}</h4>
-              <p>{item.GROUP}</p>
-              <p className="price">R$ {item.PRICE_DELIVERY}</p>
-            </div>
-          )))
+          type === 'DELIVERY' ? (
+            DadosDeliveryItems.filter((item) => (item.ACTIVE === "TRUE" || item.ACTIVE === "VERDADEIRO") && (item.PRICE_DELIVERY !== 0)).map((item, idx) => (
+              <div className="item" key={idx}>
+                <h4>{item.ITEM}</h4>
+                <p>{item.GROUP}</p>
+                <p className="price">R$ {item.PRICE_DELIVERY}</p>
+              </div>
+            ))
+          ) : (
+            DadosDeliveryItems.filter((item) => (item.ACTIVE === "TRUE" || item.ACTIVE === "VERDADEIRO") && (item.PRICE_MENU !== 0)).map((item, idx) => (
+              <div className="item" key={idx}>
+                <h4>{item.ITEM}</h4>
+                <p>{item.GROUP}</p>
+                <p className="price">R$ {item.PRICE_MENU}</p>
+              </div>
+            ))
+          )
+        )
       }
       </div>
       {
@@ -84,3 +118,20 @@ const ListaDeliveryItems = () => {
 }
 
 export default ListaDeliveryItems;
+
+// search ? (
+//           DadosDeliveryItems.filter((item) => (item.ACTIVE === "TRUE" || item.ACTIVE === "VERDADEIRO") && (item.PRICE_DELIVERY !== 0) && ((item.ITEM.toLowerCase().includes(searchWord.toLowerCase())) || (item.GROUP.toLowerCase().includes(searchWord.toLowerCase())))).map((item, idx) => (
+//             <div className="item" key={idx}>
+//               <h4>{item.ITEM}</h4>
+//               <p>{item.GROUP}</p>
+//               <p className="price">R$ {item.PRICE_DELIVERY}</p>
+//             </div>
+//           ))
+//         ) : (
+//           DadosDeliveryItems.filter((item) => (item.ACTIVE === "TRUE" || item.ACTIVE === "VERDADEIRO") && (item.PRICE_DELIVERY !== 0)).map((item, idx) => (
+//             <div className="item" key={idx}>
+//               <h4>{item.ITEM}</h4>
+//               <p>{item.GROUP}</p>
+//               <p className="price">R$ {item.PRICE_DELIVERY}</p>
+//             </div>
+//           )))
